6ade1331315d3e506cf91eaeeaf2c98263d32747
今村一貴 <imamurakazuki@imamurakazutakanoMacBook-Pro.local>
2021-09-06 19:03:43 +0900

    css

--- js/faq.js
+++ js/faq.js
@@ -1,19 +1,22 @@
 
 
 $(function () {
-  $(".c-faq__wrap__titile").click(function () {
+  $(".c-faq__wrap__title").click(function () {
 
 
 
 
-    $(".c-faq__wrap__titile").not(this).next().slideUp(300);
-    $(".is-border").toggleClass("u-border")
-
-    // $("c-faq__wrap__titile u-border").css('display', 'none');
-
+    $(".c-faq__wrap__title").not(this).next().slideUp(300);
+    // $(".c-faq__wrap__title").toggleClass("u-border")
+    // $('.op_plus').toggleClass('active');
+    // thisは押したボタンのみってこと
+    // .c-faq__wrap__titleを押した時に.op_plusを探してそこのみにクラス名をつける
+    $(this).find(".op_plus").toggleClass('active');
+  
 
 
     $(this).next().slideToggle(300);
+  
 
 
   });
